a664a1b703901f7d97d12e149519c5f0dceab0d5
enheragu <enrique.he.ag@gmail.com>
2022-10-20 14:12:37 +0200

    Update configu from nav and cartpographer, to be tunned

--- config/cartographer_husky.lua
+++ config/cartographer_husky.lua
@@ -25,7 +25,7 @@ options = {
   provide_odom_frame = false,
   publish_frame_projected_to_2d = false,
   use_odometry = true,
-  use_nav_sat = false,
+  use_nav_sat = true,
   use_landmarks = false,
   num_laser_scans = 0,
   num_multi_echo_laser_scans = 0,
@@ -33,7 +33,7 @@ options = {
   num_point_clouds = 1,
   lookup_transform_timeout_sec = 1.,
   submap_publish_period_sec = 0.3,
-  publish_tracked_pose = true,
+  publish_tracked_pose = false,
   pose_publish_period_sec = 5e-3,
   trajectory_publish_period_sec = 30e-3,
   rangefinder_sampling_ratio = 1.,
@@ -54,8 +54,8 @@ TRAJECTORY_BUILDER_2D.min_range = 0.5
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 2.
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.imu_gravity_time_constant = 2
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 10
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 15
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 15
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 10
 
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15.)
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 3.
@@ -67,13 +67,13 @@ POSE_GRAPH.optimization_problem.huber_scale = 1e2
 ------------Global SLAM------------
 POSE_GRAPH.optimize_every_n_nodes = 100 -- Decrease
 POSE_GRAPH.global_sampling_ratio = 0.003 -- Decrease
-POSE_GRAPH.constraint_builder.sampling_ratio = 0.4 -- Decrease
-POSE_GRAPH.constraint_builder.min_score = 0.85 -- Increase
-POSE_GRAPH.global_constraint_search_after_n_seconds = 30 -- Increase
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.30 -- Decrease
+POSE_GRAPH.constraint_builder.min_score = 0.8 -- Increase
+POSE_GRAPH.global_constraint_search_after_n_seconds = 40 -- Increase
 
 ---------Global/Local SLAM---------
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 100 -- Decrease
-TRAJECTORY_BUILDER_2D.max_range = 20. -- Decrease
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 90 -- Decrease
+TRAJECTORY_BUILDER_2D.max_range = 10. -- Decrease
 
 -------------------------------------------------------------------------------------
 
