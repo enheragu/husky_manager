44670c9e9daaa628cb8adae1c10330ad6e5d194d
enheragu <enrique.he.ag@gmail.com>
2022-10-26 16:16:13 +0200

    Cartographer config with correct local SLAM

--- config/cartographer_husky.lua
+++ config/cartographer_husky.lua
@@ -37,43 +37,44 @@ options = {
   pose_publish_period_sec = 5e-3,
   trajectory_publish_period_sec = 30e-3,
   rangefinder_sampling_ratio = 1.,
-  odometry_sampling_ratio = 0.5,
+  odometry_sampling_ratio = 1.,
   fixed_frame_pose_sampling_ratio = 1.,
   imu_sampling_ratio = 1.,
   landmarks_sampling_ratio = 1.,
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
-
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
 
-TRAJECTORY_BUILDER_2D.max_z = 1
-TRAJECTORY_BUILDER_2D.min_z = -0.4
-TRAJECTORY_BUILDER_2D.min_range = 0.5
 
-TRAJECTORY_BUILDER_2D.missing_data_ray_length = 2.
-TRAJECTORY_BUILDER_2D.use_imu_data = false
-TRAJECTORY_BUILDER_2D.imu_gravity_time_constant = 2
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 10
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 15
+-- TRAJECTORY_BUILDER_2D.missing_data_ray_length = 2.
+ TRAJECTORY_BUILDER_2D.use_imu_data = false
+-- TRAJECTORY_BUILDER_2D.imu_gravity_time_constant = 2
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 2e2
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 6e2
 
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15.)
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 3.
+-- POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15.)
+-- POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 3.
 
-POSE_GRAPH.optimization_problem.huber_scale = 1e2
+-- POSE_GRAPH.optimization_problem.huber_scale = 1e2
 
 -----------------TUNE THESE PARAMETERS FOR LOW LATENCY-------------------------------
 
 ------------Global SLAM------------
-POSE_GRAPH.optimize_every_n_nodes = 100 -- Decrease
-POSE_GRAPH.global_sampling_ratio = 0.003 -- Decrease
-POSE_GRAPH.constraint_builder.sampling_ratio = 0.4 -- Decrease
-POSE_GRAPH.constraint_builder.min_score = 0.85 -- Increase
-POSE_GRAPH.global_constraint_search_after_n_seconds = 30 -- Increase
+POSE_GRAPH.optimize_every_n_nodes = 0 -- Decrease
+-- POSE_GRAPH.global_sampling_ratio = 0.003 -- Decrease
+-- POSE_GRAPH.constraint_builder.sampling_ratio = 0.4 -- Decrease
+-- POSE_GRAPH.constraint_builder.min_score = 0.85 -- Increase
+-- POSE_GRAPH.global_constraint_search_after_n_seconds = 30 -- Increase
 
 ---------Global/Local SLAM---------
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 100 -- Decrease
-TRAJECTORY_BUILDER_2D.max_range = 20. -- Decrease
+-- TRAJECTORY_BUILDER_2D.submaps.num_range_data = 50 -- Decrease
+-- TRAJECTORY_BUILDER_2D.max_range = 30. -- Decrease
+TRAJECTORY_BUILDER_2D.min_range = 0.5
+TRAJECTORY_BUILDER_2D.max_z = 1
+TRAJECTORY_BUILDER_2D.min_z = -0.45
+
+-- TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.025
 
 -------------------------------------------------------------------------------------
 
